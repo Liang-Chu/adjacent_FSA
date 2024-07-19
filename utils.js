bd554766e61e6df7868c887c65ebe66f4cdcaef7
liang chu <chux2890@mylaurier.ca>
2024-07-19 18:37:33 -0400

    minor bug fix

--- utils.js
+++ utils.js
@@ -1,6 +1,7 @@
 const grouped_fsa_adjacency = require("./grouped_fsa_adjacency.json");
 
 function getAdjacentFsas(fsa, level = 1, visited = new Set()) {
+  fsa=fsa.toUpperCase();
   if (level < 1) return [];
   const f = fsa[0]; // Extract the first character (province)
   const sa = fsa.slice(1, 3); // Extract the second and third characters (SA/group)
